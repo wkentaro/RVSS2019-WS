2a256fd486c7a852c274e7f8761a615416ea4318
Kentaro Wada <www.kentaro.wada@gmail.com>
2018-12-18 16:18:38 +1000

    Update deploy0.py

--- on_robot/deploy/deploy0.py
+++ on_robot/deploy/deploy0.py
@@ -33,25 +33,35 @@ ppi.set_velocity(0,0)
 
 try:
     angle = 0
+    Kd = 20
+    Ka = 20
     while True:
         # read image from camera 
         image = ppi.get_image()
         # resize to fit network 
+        image = image[110:, :]
         image = cv2.resize(image, (84,84))
         # preprocess
         input_img = preprocess_img(image,transform)
         # get steering prediction 
         steer = net(input_img).data.numpy()              
         
-        print(steer)
+        steer_class = np.argmax(steer)
+        angle = steer_class / 10. - 0.5
 
-        Kd = 15 
-        Ka = 25
-        left  = int(Kd + Ka*steer)
-        right = int(Kd - Ka*steer)
+        if steer_class in [5]:
+            Kd = min(28, Kd + 4)
+        elif steer_class in [4, 6]:
+            Kd = min(24, Kd + 2)
+        else:
+            Kd = 20
+
+        print(steer_class, angle, Kd, Ka)
+
+        left  = int(round(Kd + Ka * angle))
+        right = int(round(Kd - Ka * angle))
         
-        ppi.set_velocity(left,right)
-        
+        ppi.set_velocity(left, right)
         
 except KeyboardInterrupt:
     ppi.set_velocity(0,0)
